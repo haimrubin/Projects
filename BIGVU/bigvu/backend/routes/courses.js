9d35e19f46f2a645c88a86976dfa64c9a26e4795
haimrubin <73288124+haimrubin@users.noreply.github.com>
2023-05-31 22:27:21 +0300

    .

--- BIGVU/bigvu/backend/routes/courses.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const courseController = require("../controller/course");
-
-router.get("/courses-list", courseController.getCoursesList);
-router.get("/course/:id", courseController.getCourse);
-
-module.exports = router;
